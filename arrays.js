00c89d09bb008c71d724ea508bba170a6cd00f97
Vitor Carv <94801330+vitorcarvdev@users.noreply.github.com>
2024-02-13 12:09:02 -0300

    Arrays e eventos
    Estudando sobre arrays e eventos

---
+++ arrays.js
@@ -0,0 +1,11 @@
+// forma mais comum de fazer
+const listaCompras = ["Arroz","Feijão","Macarrão","Leite"];
+
+// imprimindo
+listaCompras[0]; // arroz ou
+console.log(listaCompras); // mostra todos os itens do array
+
+// Outra forma de criar este mesmo Array
+const ListaCompras2 = new Array("Arroz","Feijão","Macarrão","Leite");
+
+// dentro dos arrays tambem é possivel colocar (Variaveis, objetos, e outros)
---
+++ eventos.js
@@ -0,0 +1,42 @@
+/*
+
+onload: Disparado quando documento é carregado
+onunload: Disparado quando documento é descarregado de janela ou de frame
+onsubmit: Disparado quando formulário é submetido
+onreset: Disparado quando formulário é "limpado" via botão de reset
+onselect: Disparado quando texto é selecionado numa area de entrada de texto
+onblur: Disparado quando elemento de entrada do formulário perde o foco, i.é, qdo texto fora do elemento é selecionado
+onchange: Disparado quando elemento perde o foco e foi modificado
+onclick: Disparado quando botão de formulário é pressionado ou link é selecionado via click do mouse
+ondblclick: Idem, mas click duplo do mouse
+onfocus: Disparado quando o elemento recebe foco: clicando o mouse dentro do elemento ou entrando no mesmo via Tab
+onmousedown: Disparado quando mouse é pressionado enquanto está sobre um elementro
+onmouseup: Idem, mas quando mouse é despressionado
+onmousemove: Disparado quando mouse é movido enquanto sobre elemento
+onmouseout: Disparado quando mouse é movido fora do elemento onde estava
+onmouseover: Disparado quando cursor do mouse é movido sobre elemento
+onkeydown: Disparado quando tecla é pressionada.
+onkeypress: Disparado quando tecla é pressionada e liberada.
+onkeyup: Disparado quando tecla pressionada é liberada.
+
+*/
+
+
+
+
+
+
+// posso criar variaveis com var, let, cont (melhor: let e const)
+
+let teste = "Minha String";
+
+//------------------------------------------------------
+// const precisa ter um resultado fixo
+const teste2 = 10; // inicialmente 10 será aceito
+teste2 = 11; // mas se o resultado mudar vai gerar erro
+
+
+console.log(teste2);
+// bom em caso de variaveis que nao podem ser alteradas
+
+
